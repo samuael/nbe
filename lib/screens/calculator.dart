7b05065fb7f2fd2056d3efae5315bd62b3323671
Estif-anon <amsaluestifanos@gmail.com>
2025-09-29 11:06:35 -0700

    Bottom navigation implemented

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
