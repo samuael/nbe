c0495d786e8777cb423a41bc86f171157036f78f
Estif-anon <amsaluestifanos@gmail.com>
2025-09-29 14:07:02 -0700

    getPurchaseRate not working

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -10,6 +14,71 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   final TextEditingController _weightController = TextEditingController();
   final TextEditingController _specificGravityController =
       TextEditingController();
+  double purchaseRate = 0;
+  String demoText = '';
+  final url = 'https://nbe.gov.et/exchange/gold-purchasing-rate/';
+
+  bool areSameDates(DateTime day1, DateTime day2) {
+    return day1.toIso8601String().substring(0, 10) ==
+        day2.toIso8601String().substring(0, 10);
+  }
+
+  Future<void> getPurchasingRate() async {
+    // final cached = await SharedPreferences.getInstance();
+
+    // DateTime? lastUpdated = DateTime.tryParse(
+    //   cached.getString('lastUpdated') ?? '',
+    // );
+
+    DateTime? lastUpdated = null;
+
+    if (lastUpdated == null || !areSameDates(lastUpdated, DateTime.now())) {
+      final parsed = Uri.parse(url);
+      final response = await http.get(
+        parsed,
+        headers: {
+          "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+              "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Accept":
+              "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9",
+        },
+      );
+
+      if (response.statusCode == 200) {
+        dom.Document document = parser.parse(response.body);
+        final table = document.querySelector('table');
+        if (table == null) {
+          print('No table');
+          return;
+        }
+        final rows = table.querySelectorAll('tr');
+        if (rows.isEmpty) {
+          print('no rows');
+          return;
+        } else {
+          print(rows.length);
+          return;
+        }
+        final firstDataRow = rows.firstWhere(
+          (row) => row.querySelectorAll('td').isNotEmpty,
+        ); //24 Karat's
+        if (firstDataRow.querySelectorAll('td').isEmpty) {
+          print('no table data');
+          return;
+        }
+        final loadedPurchaseRate = firstDataRow.querySelectorAll('td')[0];
+        print(loadedPurchaseRate.text);
+        setState(() {
+          demoText = loadedPurchaseRate.text;
+        });
+      } else {
+        print('Failed to load page');
+        print(response.statusCode);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +108,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                     ),
                     SizedBox(height: 15),
                     Text(
-                      ' 170,000 EtB per gm',
+                      '$purchaseRate EtB per gram $demoText',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         backgroundColor: Colors.grey[400],
@@ -164,15 +233,21 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             Padding(
               padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 0),
               child: TextField(
-                controller: _weightController,
+                controller: _specificGravityController,
                 keyboardType: TextInputType.numberWithOptions(),
-                decoration: InputDecoration(label: Text('Weight in grams')),
+                decoration: InputDecoration(
+                  label: Text('Specific gravity in cm^3'),
+                ),
               ),
             ),
             SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {},
               child: Text('Calculate', style: TextStyle(fontSize: 20)),
+            ),
+            ElevatedButton(
+              onPressed: getPurchasingRate,
+              child: Text('Get Today\'s Rate'),
             ),
           ],
         ),
