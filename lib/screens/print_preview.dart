7adf3bc054dbabccbcf21b592e176ba454a739ef
Estif-anon <amsaluestifanos@gmail.com>
2025-10-05 09:52:57 -0700

    Fix saveToPdf function. Implement Saving to app directory

--- lib/screens/print_preview.dart
+++ lib/screens/print_preview.dart
@@ -1,101 +1,101 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:nbe/libs.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:pdf/pdf.dart';
-import 'package:printing/printing.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart';
 
-class PrintPreviewScreen extends StatelessWidget {
+class PrintPreviewScreen extends StatefulWidget {
   final List<Transaction> transactions;
   const PrintPreviewScreen({required this.transactions, super.key});
+
+  @override
+  State<PrintPreviewScreen> createState() => _PrintPreviewScreenState();
+}
+
+class _PrintPreviewScreenState extends State<PrintPreviewScreen> {
+  bool isSaving = false;
 
   String getMonthYear(DateTime date) {
     return DateFormat.yMMMM().format(date);
   }
 
-  Future<bool> _requestStoragePermission(BuildContext context) async {
-    if (Platform.isAndroid) {
-      var status = await Permission.storage.status;
-      if (!status.isGranted) {
-        status = await Permission.storage.request();
-        if (!status.isGranted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-                content: Text('Storage permission is required to save PDF.')),
-          );
-          status = await Permission.manageExternalStorage.request();
-          if (!status.isGranted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                  content: Text(
-                      'Again Storage permission is required to save PDF.')),
-            );
-          }
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
   Future<void> _saveAsPdf(BuildContext context, String title,
       List<Transaction> transactions) async {
+    setState(() {
+      isSaving = true;
+    });
     final pdf = pw.Document();
 
     pdf.addPage(
       pw.Page(
+        pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
-          return pw.Column(
-            crossAxisAlignment: pw.CrossAxisAlignment.start,
-            children: [
-              pw.Text(
-                title,
-                style:
-                    pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold),
-              ),
-              pw.SizedBox(height: 16),
-              pw.Table.fromTextArray(
-                headers: ['Date', 'Karat', 'Weight (g)', 'Total Amount'],
-                data: transactions
-                    .map((tx) => [
-                          DateFormat('dd MMM yyyy').format(tx.date),
-                          '${tx.karat}K',
-                          tx.weight.toStringAsFixed(2),
-                          currencyFormatter(tx.totalAmount),
-                        ])
-                    .toList(),
-                headerStyle:
-                    pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 14),
-                cellAlignment: pw.Alignment.centerLeft,
-                cellStyle: const pw.TextStyle(fontSize: 12),
-              ),
-            ],
+          return pw.Padding(
+            padding: const pw.EdgeInsets.all(8),
+            child: pw.Column(
+              children: [
+                pw.Text(title,
+                    style: pw.TextStyle(
+                        fontSize: 16,
+                        fontWeight: pw.FontWeight.bold,
+                        color: PdfColors.black)),
+                pw.SizedBox(height: 16),
+                pw.Expanded(
+                  child: pw.ListView.separated(
+                    itemCount: transactions.length,
+                    separatorBuilder: (_, __) => pw.Divider(),
+                    itemBuilder: (context, index) {
+                      final tx = transactions[index];
+                      return pw.Row(
+                        mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                        children: [
+                          pw.Text(
+                            DateFormat('dd MMM yyyy').format(tx.date),
+                            style: const pw.TextStyle(
+                                fontSize: 14, color: PdfColors.grey600),
+                          ),
+                          pw.Text(
+                            '${tx.karat} Karat',
+                            style: const pw.TextStyle(
+                                fontSize: 14, color: PdfColors.grey600),
+                          ),
+                          pw.Text(
+                            '${tx.weight.toStringAsFixed(2)}g',
+                            style: const pw.TextStyle(
+                                fontSize: 14, color: PdfColors.grey600),
+                          ),
+                          pw.Text(
+                              '${currencyFormatter(tx.totalAmount).substring(0, (currencyFormatter(tx.totalAmount).length - 3))} Birr',
+                              style: pw.TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: pw.FontWeight.bold,
+                                  color: PdfColors.black)),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                pw.SizedBox(height: 16),
+              ],
+            ),
           );
         },
       ),
     );
 
     try {
-      final result = await FilePicker.platform.saveFile(
-        dialogTitle: 'Save PDF',
-        fileName: '${title.replaceAll(RegExp(r"[^\w\s-]"), "_")}.pdf',
-        type: FileType.custom,
-        allowedExtensions: ['pdf'],
-      );
+      //saving
+      final dir = await getApplicationDocumentsDirectory();
+      final sanitizedTitle = title.replaceAll(RegExp(r"[^\w\s-]"), "_");
+      final filePath = '${dir.path}/$sanitizedTitle.pdf';
+      final file = File(filePath);
+      await file.writeAsBytes(await pdf.save());
+      //sharing
+      await Printing.sharePdf(
+          bytes: await pdf.save(), filename: '$sanitizedTitle.pdf');
 
-      if (result != null) {
-        final bytes = await pdf.save();
-        final file = File(result);
-        await file.writeAsBytes(bytes);
-        if (context.mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('PDF saved to $result')),
-          );
-        }
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('PDF saved to: $filePath')),
+        );
       }
     } catch (e) {
       if (context.mounted) {
@@ -104,19 +104,28 @@ class PrintPreviewScreen extends StatelessWidget {
         );
       }
     }
+    setState(() {
+      isSaving = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (transactions.isEmpty) {
+    if (widget.transactions.isEmpty) {
       return Scaffold(
         appBar: AppBar(title: const Text('Print Preview')),
         body: const Center(child: Text('No transactions to display')),
       );
     }
 
-    final firstDate = transactions.first.date;
+    final firstDate = widget.transactions.first.date;
     final title = getMonthYear(firstDate);
+    final TextStyle commonLabelStyle = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: Colors.black.withOpacity(.5),
+      overflow: TextOverflow.visible,
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -134,29 +143,44 @@ class PrintPreviewScreen extends StatelessWidget {
             const SizedBox(height: 16),
             Expanded(
               child: ListView.separated(
-                itemCount: transactions.length,
+                itemCount: widget.transactions.length,
                 separatorBuilder: (_, __) => const Divider(),
                 itemBuilder: (context, index) {
-                  final tx = transactions[index];
+                  final tx = widget.transactions[index];
                   return Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(DateFormat('dd MMM yyyy').format(tx.date)),
-                      Text('${tx.karat}K'),
-                      Text('${tx.weight.toStringAsFixed(2)}g'),
-                      Text(currencyFormatter(tx.totalAmount)),
+                      Text(
+                        DateFormat('dd MMM yyyy').format(tx.date),
+                        style: commonLabelStyle,
+                      ),
+                      Text(
+                        '${tx.karat}K',
+                        style: commonLabelStyle,
+                      ),
+                      Text(
+                        '${tx.weight.toStringAsFixed(2)}g',
+                        style: commonLabelStyle,
+                      ),
+                      Text(currencyFormatter(tx.totalAmount),
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black)),
                     ],
                   );
                 },
               ),
             ),
             const SizedBox(height: 16),
-            FancyWideButton('Print', () async {
-              // await _requestStoragePermission(context);
-              if (context.mounted) {
-                await _saveAsPdf(context, title, transactions);
-              }
-            })
+            isSaving
+                ? FancyWideButton('Saving...', () {})
+                : FancyWideButton('Print', () async {
+                    // await _requestStoragePermission(context);
+                    if (context.mounted) {
+                      await _saveAsPdf(context, title, widget.transactions);
+                    }
+                  })
           ],
         ),
       ),
